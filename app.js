d2789a3848fee65d542f3484bc06e55cc9f070a3
amirfarrahi <a.m.farrahi@gmail.com>
2014-04-28 23:25:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.resource('users', require('./routes/user.js'));
 app.resource('stores', require('./routes/store.js'));
 app.resource('products', require('./routes/product.js'));
 app.resource('deals', require('./routes/deal.js'));
-http.createServer(app).listen(process.env.PORT || config.port , function(){
-  console.log('Express server listening on port ' + process.env.PORT || config.port );
+var port = Number(process.env.PORT || config.port);
+http.createServer(app).listen(port , function(){
+  console.log('Express server listening on port ' + port );
 });
+
